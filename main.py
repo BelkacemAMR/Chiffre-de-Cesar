0f343ce24e34a73a51fd75cfa9c8dd620d8322bf
Belkacem <abel-and-co@hotmail.com>
2023-04-18 22:31:52 +0200

    Version Finale

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+
+
+##############################################################################################################
 def caesar_cypher_encrypt(s, offset):
     enc_chars = str.maketrans(
         f'{chars[0]}{chars[1]}',
         f'{chars[0][offset:]}{chars[0][:offset]}{chars[1][offset:]}{chars[1][:offset]}'
     )
     return str.translate(s, enc_chars)
-
 
 def caesar_cypher_decrypt(s, offset):
     dec_chars = str.maketrans(
